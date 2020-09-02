f0fcd25d2b8fa7ae01823bc2e94301d0fd754036
SergeyZaborskiy <bekovich9119@gmail.com>
2020-09-02 18:30:39 +0300

    Created Git repo

---
+++ Worker.java
@@ -0,0 +1,20 @@
+public class Worker {
+    private OnTaskDoneListener doneCallback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener doneCallback, OnTaskErrorListener errorCallback) {
+        this.doneCallback = doneCallback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i != 33) {
+                doneCallback.onDone("Task " + i + " is done");
+            } else {
+                errorCallback.onError("Task " + i + " return error");
+            }
+        }
+    }
+
+}
